ff125573ebf862b35cc635523c35eea2945af5d1
vidarhf <vidarhf@gmail.com>
2023-07-31 10:35:36 +0200

    Simple description and title
    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,27 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}></div>
-
-      <div className={styles.center}></div>
-
+      <div className={styles.h1}>
+        <a href="https://aframe.io/">
+          <b>V</b>ercel
+        </a>
+        •
+        <a href="https://aframe.io/">
+          <b> A</b>-Frame
+        </a>
+        •
+        <a href="https://nextjs.org">
+          <b> N</b>ext.js
+        </a>
+        •
+        <a href="https://www.typescriptlang.org">
+          <b> T</b>ype<b>S</b>cript
+        </a>
+      </div>
+      <div className={styles.description}>
+        Click the link below to see an example 3D scene. Move with WASD keys and
+        mouse, or use VR headset.
+      </div>
       <div className={styles.grid}>
         <a
           href="/HelloWorld"
@@ -23,4 +40,3 @@ export default function Home() {
     </main>
   );
 }
-
